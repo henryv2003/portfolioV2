8850e94981800d60f89ecb2f2677368ced6dd41b
henryv2003 <alexandervh1986@gmail.com>
2025-12-10 23:11:52 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { NavBar } from "../components/Navbar"
+import { StarBackground } from "../components/StarBackground"
+import { ThemeToggle } from "../components/ThemeToggle"
+
+export const Home = () => {
+return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+  {/* Theme togglee */}
+  <ThemeToggle/>
+  {/* background effects */}
+  <StarBackground/>
+  {/* NavBar */}
+  <NavBar/>
+  {/* Main Content */}
+  {/* Footer */}
+</div>
+}
